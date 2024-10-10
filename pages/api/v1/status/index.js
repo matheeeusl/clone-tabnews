aa6537dd6492b78501d94716490aa60d3738e85b
Matheus Laureano <mlaureano@fullstacklabs.co>
2024-10-10 15:02:01 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows)
-  response.status(200).json({ status: "OK" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const openedConnectionsValue = openedConnections.rows[0].count;
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = parseInt(maxConnections.rows[0].max_connections);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConnectionsValue,
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test('GET to /api/v1/status returns 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const updatedAt = responseBody.updated_at;
+  expect(new Date(updatedAt).toISOString()).toEqual(updatedAt);
+
+  const database = responseBody.dependencies.database;
+  expect(database).toBeDefined();
+
+  expect(database.version).toEqual('16.0')
+  expect(database.max_connections).toEqual(100)
+  expect(database.opened_connections).toEqual(1)
+
 });
